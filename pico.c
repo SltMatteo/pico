371343a874707be834c5a203197ce8035404cb2a
SltMatteo <74387871+SltMatteo@users.noreply.github.com>
2024-10-24 18:20:23 +0200

    c kw and comments highlights

--- pico.c
+++ pico.c
@@ -22,6 +22,7 @@
 #define PICO_TAB_STOP 8
 #define PICO_QUIT_TIMES 3
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGH_STRINGS (1<<1) 
 
 enum editorKey {
 	BACKSPACE = 127, 
@@ -38,6 +39,10 @@ enum editorKey {
 
 enum editorHighLight {
 	HL_NORMAL = 0, 
+	HL_COMMENT, 
+	HL_KEYWORD1, 
+	HL_KEYWORD2, 
+	HL_STRING, 
 	HL_NUMBER, 
 	HL_MATCH
 }; 
@@ -47,6 +52,8 @@ enum editorHighLight {
 struct editorSyntax {
 	char* filetype;
 	char** filematch; 
+	char** keywords; 
+	char* singleline_comment_start; 
 	int flags; 
 }; 
 
@@ -81,11 +88,21 @@ struct editorConfig E;
 
 char* C_HL_extensions[] = { ".c", ".h", ".cpp", NULL};
 
+char *C_HL_keywords[] = {
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
+};
+
+
 struct editorSyntax HLDB[] = {
 	{
 		"c", 
 		C_HL_extensions, 
-		HL_HIGHLIGHT_NUMBERS
+		C_HL_keywords, 
+		"//", 
+		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGH_STRINGS
 	}, 
 }; 
 
@@ -226,12 +243,47 @@ void editorUpdateSyntax(erow* row) {
 
 	if (E.syntax == NULL) return; 
 
+	char** keywords = E.syntax->keywords; 
+
+	char* scs = E.syntax->singleline_comment_start; 
+	int scs_len = scs ? strlen(scs) : 0; 
+
 	int prev_sep = 1; 
+	int in_string = 0; 
 
 	int i = 0; 
 	while (i < row->rsize) {
 		char c = row->render[i]; 
-		unsigned char prev_hl = (i > 0) ? row->hl[i-1] : HL_NORMAL; 
+		unsigned char prev_hl = (i > 0) ? row->hl[i-1] : HL_NORMAL;
+
+		if (scs_len && !in_string) {
+			if (!strncmp(&row->render[i], scs, scs_len)) {
+				memset(&row->hl[i], HL_COMMENT, row->rsize - i); 
+				break; 
+			}
+		} 
+
+		if (E.syntax->flags & HL_HIGHLIGH_STRINGS) {
+			if (in_string) {
+				row->hl[i] = HL_STRING; 
+				if (c == '\\' && i + 1 < row->rsize) {
+					row->hl[i+1] = HL_STRING; 
+					i += 2;
+					continue; 
+				}
+				if (c == in_string) in_string = 0; 
+				i++; 
+				prev_sep = 1; 
+				continue; 
+			} else {
+				if (c == '"' || c == '\'') {
+					in_string = c; 
+					row->hl[i] = HL_STRING; 
+					i++; 
+					continue; 
+				}
+			}
+		}
 
 		if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
 			if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
@@ -240,9 +292,29 @@ void editorUpdateSyntax(erow* row) {
 				prev_sep = 0; 
 				continue; 
 			}
-			prev_sep = is_separator(c); 
-			i++; 
 		}
+
+		if (prev_sep) {
+			int j; 
+			for (j = 0; keywords[j]; j++) {
+				int klen = strlen(keywords[j]); 
+				int kw2 = keywords[j][klen-1] == '|';
+				if (kw2) klen--; 
+
+				if (!strncmp(&row->render[i], keywords[j], klen) && is_separator(row->render[i+klen])) {
+					memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+					i += klen; 
+					break; 
+				}
+			}
+			if (keywords[j] != NULL) {
+				prev_sep = 0; 
+				continue; 
+			}
+		}
+
+		prev_sep = is_separator(c); 
+		i++; 
 	}
 }
 
@@ -251,7 +323,15 @@ int editorSyntaxToColor(int hl) {
 		case HL_NUMBER: 
 			return 31; //red 
 		case HL_MATCH: 
+			return 34; //blue 
+		case HL_STRING:
+			return 35; //magenta
+		case HL_COMMENT: 
+			return 36; //cyan 
+		case HL_KEYWORD1: 
 			return 33; //yellow 
+		case HL_KEYWORD2: 
+			return 32; //green 
 		default: 
 			return 37; //white 
 	}
@@ -270,6 +350,11 @@ void editorSelectSyntaxHighlight(void) {
 			int is_ext = (s->filematch[i][0] == '.'); 
 			if ((is_ext && ext && !strcmp(ext, s->filematch[i])) || (!is_ext && strstr(E.filename, s->filematch[i]))) {
 				E.syntax = s; 
+
+				int filerow; 
+				for (filerow = 0; filerow < E.numrows; filerow++) {
+					editorUpdateSyntax(&E.row[filerow]); 
+				}
 				return; 
 			}
 			i++; 
